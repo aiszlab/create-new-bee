758645bdad419a30271e1367cb1e936cc5889ec2
murukal <tutu@fantufantu.com>
2022-09-20 22:44:04 +0800

    feat: add generator

--- packages/create-bee/src/libs/create.ts
+++ packages/create-bee/src/libs/create.ts
@@ -2,16 +2,8 @@
 import inquirer from 'inquirer'
 import yeoman from 'yeoman-environment'
 import chalk from 'chalk'
-// project
-import { PROJECT_TEMPLATE_CHOICE, TELEPORT_CHOICE } from '../assets/create.js'
 
-/**
- *
- */
 export const create = async () => {
-  // 选项
-  const createOptions = await inquirer.prompt([TELEPORT_CHOICE, PROJECT_TEMPLATE_CHOICE])
-  console.log('createOptions=====', createOptions)
   // 创建yeoman运行时
   const env = yeoman.createEnv()
   // 运行时：下载以来
