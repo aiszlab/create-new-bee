cf78edf387b1cf83c8bbe34f94910ebb3015ba36
murukal <tutu@fantufantu.com>
2022-09-27 22:51:57 +0800

    feat: build scripts

--- packages/bee-scripts/src/index.ts
+++ packages/bee-scripts/src/index.ts
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 import { Command } from 'commander'
+import { build } from './libs/build'
 import { start } from './libs/start'
 
 const program = new Command()
 
 program.command('start').action(async () => {
   await start().catch(() => false)
+})
+
+program.command('build').action(async () => {
+  await build().catch(() => false)
 })
 
 program.parse()
---
+++ packages/bee-scripts/src/libs/build.ts
@@ -0,0 +1,12 @@
+import { sync } from 'cross-spawn'
+
+/**
+ * 启动开发模式
+ * @react 依赖react-scripts
+ */
+export const build = async () => {
+  // 同步调用 react-scripts
+  sync('node', [require.resolve('react-scripts/bin/react-scripts.js'), 'build'], {
+    stdio: 'inherit'
+  })
+}
