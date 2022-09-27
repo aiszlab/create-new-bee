b71945da9c7cc4e2f8162825a802e93d4e783b1b
murukal <tutu@fantufantu.com>
2022-09-27 19:49:33 +0800

    feat: remove useless code

--- packages/create-bee/bin/index.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import { Command } from 'commander';
-import { create } from './libs/create.js';
-const program = new Command();
-program
-    .command('create')
-    .option('-l|--local')
-    .action((options) => create(options));
-program.parse();
