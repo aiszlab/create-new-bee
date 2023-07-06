55feadd5629ef93930e56fbb51f88c6dd1b719a3
murukal <tutu@fantufantu.com>
2023-07-06 09:32:07 +0800

    feat: 可视化初始化项目

--- packages/create-new-bee/src/libs/create.ts
+++ packages/create-new-bee/src/libs/create.ts
@@ -1,7 +1,15 @@
 import { Options } from '../typings/create.js'
+import { createRequire } from 'module'
+
+const require = createRequire(import.meta.url)
+const nextStart = require('@aiszlab/wysiwyg')
 
 /**
  * @author murukal
  * @description 脚手架创建模板的函数，集成了不少插件，慢慢看~
  */
-export const create = async (options: Options) => {}
+export const create = async (options: Options) => {
+  console.log(nextStart)
+
+  nextStart()
+}
---
+++ packages/wysiwyg/runtime.js
@@ -0,0 +1,5 @@
+const { nextStart } = require('next/dist/cli/next-start.js')
+
+console.log('1111')
+
+module.exports = nextStart
